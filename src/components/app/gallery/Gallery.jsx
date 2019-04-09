079ff19ad67a986a4f8b49ab73431bf78e4f9c7e
Nicolas Espindola <nicolasemanuelespindola@gmail.com>
2019-04-09 14:07:45 -0300

    agrego metodo para alertar cuando se llega a cierto punto de la barra de scroll

--- src/components/app/gallery/Gallery.jsx
+++ src/components/app/gallery/Gallery.jsx
@@ -1,30 +1,52 @@
-import React, { Component } from 'react';
-import Exposition from './exposition/Exposition';
-import { withStyles } from '@material-ui/core';
+import React, { Component } from "react";
+import Exposition from "./exposition/Exposition";
+import { withStyles } from "@material-ui/core";
 
 const styles = theme => ({
-    container: {
-        display: 'flex',
-        flexDirection: 'column',
-        flexGrow: '1',
-        overflow: 'auto',
-    },
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    flexGrow: "1",
+    overflow: "auto"
+  }
 });
 
 class Gallery extends Component {
+  handleViewButtonClick = post => {
+    this.props.onViewButtonClick(post);
+  };
 
-    handleViewButtonClick = (post) => {
-        this.props.onViewButtonClick(post)
-    }
+  handleScroll = event => {
+    let element = event.target;
 
-    render() {
-        const { classes } = this.props;
-        return (
-            <div className={classes.container}>
-                <Exposition postList={this.props.postList} onViewButtonClick={this.handleViewButtonClick} />
-            </div>
-        )
-    }
+    //Valor absoluto del fondo de la barra de scrolling
+    const bottomScrollValue = element.scrollHeight - element.clientHeight;
+
+    //Valor actual de la barra de scrolling
+    const currentScrollValue = element.scrollTop;
+
+    //Porcentaje limite
+    const percentage = 80 / 100;
+
+    //Limite para alertar cuando la barra de desplazamiento sobrepasa ese valor
+    const limitScrollValue = percentage * bottomScrollValue;
+
+    const limitReached = currentScrollValue >= limitScrollValue;
+
+    if (limitReached) alert("limit reached");
+  };
+
+  render() {
+    const { classes } = this.props;
+    return (
+      <div className={classes.container} onScroll={this.handleScroll}>
+        <Exposition
+          postList={this.props.postList}
+          onViewButtonClick={this.handleViewButtonClick}
+        />
+      </div>
+    );
+  }
 }
 
-export default withStyles(styles)(Gallery)
+export default withStyles(styles)(Gallery);
